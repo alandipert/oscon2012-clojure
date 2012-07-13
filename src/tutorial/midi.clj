67d4744c90ba836cf0dc57fe98d68c11d6cb0d4a
Clinton N. Dreisbach <crnixon@gmail.com>
2012-07-13 16:19:14 -0400

    Updated midi code

--- src/tutorial/midi.clj
+++ src/tutorial/midi.clj
@@ -1,26 +1,28 @@
 (ns tutorial.midi
   (:import (javax.sound.midi MidiSystem Synthesizer)))
 
+(def notes
+  [{:note 60 :duration 1}
+   {:note 62 :duration 1}
+   {:note 64 :duration 1}
+   {:note 60 :duration 1}
+   {:note 60 :duration 1}
+   {:note 62 :duration 1}
+   {:note 64 :duration 1}
+   {:note 60 :duration 1}
+   {:note 64 :duration 1}
+   {:note 65 :duration 1}
+   {:note 67 :duration 2}
+   {:note 64 :duration 1}
+   {:note 65 :duration 1}
+   {:note 67 :duration 2}])
+
 (defn play-a-note [note velocity duration]
   (with-open [synth (doto (MidiSystem/getSynthesizer) .open)]
     (let [channel (aget (.getChannels synth) 0)]
       (.noteOn channel note velocity)
-      (Thread/sleep duration))))
+      (Thread/sleep duration)
+      (.noteOff channel note))))
 
 (play-a-note 60 127 3000)
 
-
-(defn play
-  ([note] (play note 2))
-  ([note duration] (play-a-note note 127 (* 200 duration))))
-
-(def notes
-  [52 54 56 52 52 54 56 52 56 57 [59 1] 56 57 [59 1] [59 4] [61 4] [59 4] [57 4]
-   56 52 [59 4] [61 4] [59 4] [57 4] 56 52 52 47 [52 1] 52 47 [52 1]])
-
-(doseq [note notes]
-  (if (vector? note)
-    (apply play note)
-    (play note)))
-
-(vector? [1 2])
