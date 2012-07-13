aa657657b64482b1abb2f759faa5312b6cd68a61
Clinton N. Dreisbach <crnixon@gmail.com>
2012-07-13 16:17:41 -0400

    Added another exercise

--- src/tutorial/midi.clj
+++ src/tutorial/midi.clj
@@ -1,13 +1,26 @@
 (ns tutorial.midi
   (:import (javax.sound.midi MidiSystem Synthesizer)))
 
-(defn play-raw-note [channel note velocity]
-  (.noteOn channel note velocity))
-
 (defn play-a-note [note velocity duration]
   (with-open [synth (doto (MidiSystem/getSynthesizer) .open)]
     (let [channel (aget (.getChannels synth) 0)]
-      (play-raw-note channel note velocity)
+      (.noteOn channel note velocity)
       (Thread/sleep duration))))
 
 (play-a-note 60 127 3000)
+
+
+(defn play
+  ([note] (play note 2))
+  ([note duration] (play-a-note note 127 (* 200 duration))))
+
+(def notes
+  [52 54 56 52 52 54 56 52 56 57 [59 1] 56 57 [59 1] [59 4] [61 4] [59 4] [57 4]
+   56 52 [59 4] [61 4] [59 4] [57 4] 56 52 52 47 [52 1] 52 47 [52 1]])
+
+(doseq [note notes]
+  (if (vector? note)
+    (apply play note)
+    (play note)))
+
+(vector? [1 2])
